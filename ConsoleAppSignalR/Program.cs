1da4884923c3e13ab00cfdfe91bbdda55df3a4e5
Xueli Chen <v-xuelc@microsoft.com>
2020-04-16 16:05:27 +0800

    first commit

---
+++ ConsoleAppSignalR/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSignalR
+{
+    public class Program
+    {
+        public static  void Main(string[] args)
+        {
+            GetConnection().Wait();
+                
+            Console.WriteLine("Hello World!");
+        }
+
+        public static async Task GetConnection()
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/chatHub?token=123")
+                .Build();
+            
+            await connection.StartAsync();
+        }
+
+    }
+}
---
+++ SignalRDemo2_1/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRDemo2_1.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var group = Context.GetHttpContext().Request.Query["token"];
+
+            string value = !string.IsNullOrEmpty(group.ToString()) ? group.ToString() : "default";
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, value);
+            await base.OnConnectedAsync();
+        }
+    }
+}
